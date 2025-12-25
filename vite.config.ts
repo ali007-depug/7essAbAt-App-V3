f86c478d9d48ffcd6789ffbd54972c4bc7a4af51
ali007-depug <aliroma849@gmail.com>
2025-12-25 14:22:17 +0200

    fix pwa image

--- vite.config.ts
+++ vite.config.ts
@@ -20,14 +20,15 @@ export default defineConfig({
         background_color: "#0f172a",
         display: "standalone",
         start_url: "/",
+        scope: "/",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
